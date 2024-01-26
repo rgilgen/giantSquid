9b767de690af28908ebe3d91d7161ff557a8767e
rgilgen <rgilgen@live.com>
2024-01-26 11:07:39 +0100

    Spock setup

---
+++ src/main/java/ch/css/produkt/kata/BingoWinnerRetriever.java
@@ -0,0 +1,9 @@
+package ch.css.produkt.kata;
+
+import java.io.File;
+
+public class BingoWinnerRetriever {
+    public int getWinnerPoints(File game){
+        return 1;
+    }
+}
---
+++ src/main/test/ch/css/produkt/kata/BingoWinnerRetrieverTest.groovy
@@ -0,0 +1,9 @@
+package ch.css.produkt.kata
+
+import spock.lang.Specification
+
+class BingoWinnerRetrieverTest extends Specification {
+
+    def "GetWinnerPoints"() {
+    }
+}
